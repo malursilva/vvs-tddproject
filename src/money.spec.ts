4a9ad637f31cbe8cab574f454d34a09785647578
Malu Silva <malursilva1712@gmail.com>
2021-08-03 23:01:20 -0300

    test: remove dollar and euro classes

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,4 +1,4 @@
-import { Dollar, Euro, Money } from './money'
+import { Money } from './money'
 
 describe('Money', () => {
     it('should correctly handle dollar multiplication', () => {
@@ -7,19 +7,10 @@ describe('Money', () => {
         expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
     })
 
-    it('should correctly handle euro multiplication', () => {
-        const five: Money  = Money.euro(5)
-        expect(five.times(2).equals(Money.euro(10))).toBeTruthy()
-        expect(five.times(3).equals(Money.euro(15))).toBeTruthy()
-    })
-
     it('should correctly handle money equality', () => {
         expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
         expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
-        expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy()
-        expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy()
         expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
-        expect(new Money(5, 'USD').equals(Money.dollar(5))).toBeTruthy()
     })
 
     it('should correctly handle currencies', () => {
